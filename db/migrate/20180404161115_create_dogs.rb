406f605781d728312da97b96abb877c17d7105a6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-04 16:13:53 +0000

    automatically backed up by learn

--- db/migrate/20180404161115_create_dogs.rb
+++ db/migrate/20180404161115_create_dogs.rb
@@ -1,14 +1,14 @@
 class CreateDogs < ActiveRecord::Migration[5.1]
-  def up
+ def up
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
   end
 
-  def down
-    drop_table :dogs
+ def down
+  drop_table :dogs
   end 
-  end
+ end
 
 end
